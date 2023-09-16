5019bf55db8d13a1fda833051d464cd9730b39ac
Your Name <you@example.com>
2023-09-16 11:20:51 +0530

    first commit

---
+++ src/components/movieList/movieList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./movieList.css";
+import { useParams } from "react-router-dom";
+import Cards from "../card/card";
+
+const MovieList = () => {
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [type]);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=4ee7d9403b84ff14236672c378cc1d19&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  };
+  return (
+    <div className="movie_list">
+      <h2 className="list_title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+      <div className="list_cards">
+        {movieList.map((movie) => (
+          <Cards movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
